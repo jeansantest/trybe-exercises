646b1b8deed7d102abc3d5befcaed2fb762609f3
Jean Esteves <jean.esteves2001@gmail.com>
2021-03-24 16:19:02 -0300

    Exercicios 8 e 9 feitos

--- bloco_4/dia_1/exercise/scripts2.js
+++ bloco_4/dia_1/exercise/scripts2.js
@@ -1,4 +1,4 @@
-let notaEmPorcentagem = -2;
+let notaEmPorcentagem = 100;
 
 if (notaEmPorcentagem > 100 || notaEmPorcentagem < 0) {
   console.log("Ocorreu algum erro")
@@ -14,4 +14,24 @@ if (notaEmPorcentagem > 100 || notaEmPorcentagem < 0) {
   console.log("E")
 } else if (notaEmPorcentagem < 50) {
   console.log("F")
+}
+
+let a = 3;
+let b = 3;
+let c = 3;
+
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+let d = 2;
+let e = 2;
+let f = 3;
+
+if (d % 2 == 1 || e % 2 == 1 || f % 2 == 1) {
+  console.log(true)
+} else {
+  console.log(false)
 }
