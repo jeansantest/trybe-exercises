4a250f8aefe3ad7b8f9cb6ed8d78a1daa91454db
Jean Esteves <jean.esteves2001@gmail.com>
2021-03-24 19:56:50 -0300

    Estrutura exercicio 11 ++

--- bloco_4/dia_1/exercise/scripts3.js
+++ bloco_4/dia_1/exercise/scripts3.js
@@ -10,7 +10,7 @@ if (lucro >= 0) {
   console.log('Não tem lucro, algo deu errado')
 }
 
-let salarioBruto = 2500;
+let salarioBruto = 6000;
 let aliquota8 = salarioBruto * (8 / 100);
 let aliquota9 = salarioBruto * (9 / 100);
 let aliquota11 = salarioBruto * 11 / 100;
@@ -42,5 +42,16 @@ if (resultado8 <= 1903.98) {
   console.log(resultado9)
 } else if (resultado9 <= 2826.65) {
   console.log(resultado9 * 7.5 / 100 - 142.80)
+} else if (resultado9 <= 3751.05) {
+  console.log(resultado9 * 15 / 100 - 354.80)
+} else if (resultado11 <= 3751.05) {
+  console.log(resultado11 * 15 / 100 - 354.80)
+} else if (resultado11 <= 4664.68) {
+  console.log(resultado11 * 22.5 / 100 - 636.13)
+} else if (resultadoMax <= 4664.68) {
+  console.log(resultadoMax * 22.5 / 100 - 636.13)
+} else if (resultado11 > 4664.68) {
+  console.log(resultadoMax * 27.5 / 100 - 869.36)
+} else if (resultadoMax > 4664.68) {
+  console.log(resultadoMax * 27.5 / 100 - 869.36)
 }
-
