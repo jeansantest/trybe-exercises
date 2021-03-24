9e299421a4a5f82558d72f0064bd162354286713
Jean Esteves <jean.esteves2001@gmail.com>
2021-03-24 11:42:56 -0300

    Exercicio de fixaçao da parte 2

---
+++ bloco_4/dia_1/treinos/scripts.js
@@ -0,0 +1,8 @@
+const name = "Jean";
+const birthCity = "Volta Redonda";
+let birthYear = "2001";
+birthYear = "2020";
+birthCity = "VR";
+//Dará erro por ser uma constante, não uma variável.
+
+console.log(name, birthCity, birthYear)
