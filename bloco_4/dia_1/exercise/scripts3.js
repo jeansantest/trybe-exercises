bc6333728bc4467372a63cab03e09b5de415d95f
Jean Esteves <jean.esteves2001@gmail.com>
2021-03-24 20:31:30 -0300

    Exercicio 11 feito e codigo reorganizado (adicionado referencia)

--- bloco_4/dia_1/exercise/scripts3.js
+++ bloco_4/dia_1/exercise/scripts3.js
@@ -10,48 +10,44 @@ if (lucro >= 0) {
   console.log('Não tem lucro, algo deu errado')
 }
 
-let salarioBruto = 6000;
-let aliquota8 = salarioBruto * (8 / 100);
-let aliquota9 = salarioBruto * (9 / 100);
-let aliquota11 = salarioBruto * 11 / 100;
+let salarioBruto = 4000;
+let aliquota8 = 8 / 100;
+let aliquota9 = 9 / 100;
+let aliquota11 = 11 / 100;
 let aliquotaMax = 570.88;
-let resultado8 = salarioBruto - aliquota8;
-let resultado9 = salarioBruto - aliquota9;
-let resultado11 = salarioBruto - aliquota11;
-let resultadoMax = salarioBruto - aliquotaMax;
+
+let INSS;
+let IR; 
 
 if (salarioBruto < 0) {
   console.log('Algo deu errado')
 } else if (salarioBruto <= 1556.94) {
-  console.log(resultado8)
-  salarioBruto - aliquota8;
+  INSS = salarioBruto * aliquota8;
 } else if (salarioBruto <= 2594.92) {
-  console.log(resultado9)
-  salarioBruto - aliquota9;
+  INSS = salarioBruto * aliquota9;
 } else if (salarioBruto <= 5189.82) {
-  console.log(resultado11)
-  salarioBruto - aliquota11;
+  INSS = salarioBruto * aliquota11;
 } else {
-  console.log(resultadoMax)
-  salarioBruto - aliquotaMax;
+  INSS = salarioBruto * aliquotaMax;
 }
 
-if (resultado8 <= 1903.98) {
-  console.log(resultado8)
-} else if (resultado9 <= 1903.98) {
-  console.log(resultado9)
-} else if (resultado9 <= 2826.65) {
-  console.log(resultado9 * 7.5 / 100 - 142.80)
-} else if (resultado9 <= 3751.05) {
-  console.log(resultado9 * 15 / 100 - 354.80)
-} else if (resultado11 <= 3751.05) {
-  console.log(resultado11 * 15 / 100 - 354.80)
-} else if (resultado11 <= 4664.68) {
-  console.log(resultado11 * 22.5 / 100 - 636.13)
-} else if (resultadoMax <= 4664.68) {
-  console.log(resultadoMax * 22.5 / 100 - 636.13)
-} else if (resultado11 > 4664.68) {
-  console.log(resultadoMax * 27.5 / 100 - 869.36)
-} else if (resultadoMax > 4664.68) {
-  console.log(resultadoMax * 27.5 / 100 - 869.36)
+let salarioBase = salarioBruto - INSS
+
+if (salarioBase <= 1903.98) {
+  IR = 0;
+} else if (salarioBase <= 2826.65) {
+  IR = salarioBase * 7.5 / 100 - 142.80
+} else if (salarioBase <= 3751.05) {
+  IR = salarioBase * 15 / 100 - 354.80
+} else if (salarioBase <= 4664.68) {
+  IR = salarioBase * 22.5 / 100 - 636.13
+} else if (salarioBase > 4664.68) {
+  IR = salarioBase * 27.5 / 100 - 869.36
 }
+
+let salarioLiquido = salarioBase - IR;
+
+console.log(salarioLiquido)
+
+/*Pra conseguir dar uma organizada melhor no meu código dei uma estudada no repositório do Matheus Inacio:
+https://github.com/heyset/Trybe/pull/7 */
