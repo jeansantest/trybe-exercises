e2f83cadce1ffdb0adc61ce5e902002c0a471f7c
Jean Esteves <jean.esteves2001@gmail.com>
2021-03-26 18:37:06 -0300

    exercicio 1 feito

---
+++ bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,9 @@
+let n = 5;
+
+for(let index = 0; index < n; index++) {
+  let linhaDeAsteriscos = '';
+for(let linha = 0; linha < n; linha++) {
+  linhaDeAsteriscos += '*';
+}
+console.log(linhaDeAsteriscos)
+}
