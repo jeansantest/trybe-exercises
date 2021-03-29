333a4e15c946bf65bd7b24982d5d7ffc312329c2
Jean Esteves <jean.esteves2001@gmail.com>
2021-03-29 15:48:40 -0300

    for/in pratica 2

--- bloco_4/dia_4/for-in/for-in.js
+++ bloco_4/dia_4/for-in/for-in.js
@@ -10,3 +10,14 @@ for(let key in names) {
   console.log('Olá, ' + names[key])
 }
 
+//prática 2
+
+let carro = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+}
+
+for(let key in carro) {
+  console.log(key + ': ' + carro[key])
+}
