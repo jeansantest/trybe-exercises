878a02447c436ebceb011f1ab27fc238cb44f1ac
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-05 19:54:13 -0300

    exercicio da parte 3

--- bloco_5/dia_1/part3/script.js
+++ bloco_5/dia_1/part3/script.js
@@ -1,13 +1,17 @@
-let page = document.querySelectorAll('*')
-for(let i = 0; i < page.length; i += 1) {
-  page[i].style.color = 'black'
+let header = document.querySelector('header')
+header.style.backgroundColor = 'green'
+
+let emergencyTasks = document.querySelectorAll('.emergency-tasks')[0]
+emergencyTasks.style.backgroundColor = 'pink'
+let emergencyH3 = document.querySelectorAll('.emergency-tasks h3');
+
+for(let key of emergencyH3) {
+  key.style.backgroundColor = 'magenta'
 }
 
-let emergencyTasks = document.querySelectorAll('.emergency-tasks')
-let divEmergency = document.querySelectorAll('section.emergency-tasks div h3')
-
-for(let i = 0; i < emergencyTasks.length; i += 1) {
-  emergencyTasks[i].style.backgroundColor = 'pink'
-  divEmergency[i].style.backgroundColor = 'magenta'
+let noEmergency = document.querySelectorAll('.no-emergency-tasks')[0]
+noEmergency.style.backgroundColor = 'yellow'
+let noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks h3');
+for(let key of noEmergencyH3) {
+  key.style.backgroundColor = 'black'
 }
-document.divEmergency.style.color = 'red'
