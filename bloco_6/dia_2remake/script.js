551fc2feb0b6c7af46646be58167542b2a19fabd
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-18 18:21:30 -0300

    documento formatado

--- bloco_6/dia_2remake/script.js
+++ bloco_6/dia_2remake/script.js
@@ -39,21 +39,21 @@ function createOptionStates() {
 }
 createOptionStates();
 
-document.getElementById('date-input').DatePickerX.init({
-  format: 'dd/mm/yyyy'
+document.getElementById("date-input").DatePickerX.init({
+  format: "dd/mm/yyyy",
 });
 
-new window.JustValidate('.js-form', {
+new window.JustValidate(".js-form", {
   rules: {
     name: {
       required: true,
       minLength: 3,
-      maxLength: 40
+      maxLength: 40,
     },
     email: {
       required: true,
       email: true,
-      maxLength: 50
+      maxLength: 50,
     },
     cpf: {
       required: true,
@@ -79,11 +79,11 @@ new window.JustValidate('.js-form', {
     },
     office: {
       required: true,
-      maxLength: 40
+      maxLength: 40,
     },
     description: {
       required: true,
-      maxLength: 500
+      maxLength: 500,
     },
     date: {
       required: true,
@@ -91,46 +91,46 @@ new window.JustValidate('.js-form', {
   },
   messages: {
     name: {
-      required: 'O campo de nome é obrigatório.',
-      maxLength: 'O limite é de 40 caracteres.'
+      required: "O campo de nome é obrigatório.",
+      maxLength: "O limite é de 40 caracteres.",
     },
     email: {
-      required: 'O campo de email é obrigatório.',
-      email: 'O email digitado não é válido.',
-      maxLength: 'O limite é de 50 caracteres.'
+      required: "O campo de email é obrigatório.",
+      email: "O email digitado não é válido.",
+      maxLength: "O limite é de 50 caracteres.",
     },
     cpf: {
-      required: 'O campo de CPF é obrigatório.',
-      maxLength: 'O limite é de 11 caracteres.'
+      required: "O campo de CPF é obrigatório.",
+      maxLength: "O limite é de 11 caracteres.",
     },
     address: {
-      required: 'O campo de endereço é obrigatório',
-      maxLength: 'O limite é de 200 caracteres',
+      required: "O campo de endereço é obrigatório",
+      maxLength: "O limite é de 200 caracteres",
     },
     city: {
-      required: 'O campo de cidade é obrigatório',
-      maxLength: 'O limite é de 28 caracteres',
+      required: "O campo de cidade é obrigatório",
+      maxLength: "O limite é de 28 caracteres",
     },
     state: {
-      required: 'Selecione algum estado',
+      required: "Selecione algum estado",
     },
     radio: {
-      required: 'Selecione um tipo de imóvel'
+      required: "Selecione um tipo de imóvel",
     },
     text: {
-      required: 'O campo de texto é obrigatório',
-      maxLength: 'O limite é de 1000 caracteres',
+      required: "O campo de texto é obrigatório",
+      maxLength: "O limite é de 1000 caracteres",
     },
     office: {
-      required: 'O campo de cargo é obrigatório',
-      maxLength: 'O limite é de 40 caracteres',
+      required: "O campo de cargo é obrigatório",
+      maxLength: "O limite é de 40 caracteres",
     },
     description: {
-      required: 'O campo de descrição do cargo é obrigatório',
-      maxLength: 'O limite é de 500 caracteres',
+      required: "O campo de descrição do cargo é obrigatório",
+      maxLength: "O limite é de 500 caracteres",
     },
     date: {
-      required: 'Selecione uma data',
+      required: "Selecione uma data",
     },
   },
-  });
+});
