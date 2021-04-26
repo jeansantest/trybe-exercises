8fe6e59df015c916846cee1ef7d4460fb6e8c1fe
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-26 15:48:14 -0300

    exercicio parte 2.1 e 2.2

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -11,3 +11,9 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 const arrSorted = oddsAndEvens.sort((a,b) => a - b).join(',');
 
 console.log(`Os números ${arrSorted} se encontram ordenados de forma crescente!`);
+
+const factorial = n => n < 2 ? 1 : factorial(n-1) * n;
+console.log(factorial(5))
+
+const longestWord = s => s.split(' ').sort((a,b)=>a.length > b.length ? -1 : a.length < b.length ? 1 : 0).slice(0,1).join('');
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"))
