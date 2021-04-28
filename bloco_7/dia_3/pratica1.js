cf17e0fb6dd31c2e63264bacf59ab5b5f6daff44
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-28 12:41:25 -0300

    exercicio 2 feito

--- bloco_7/dia_3/pratica1.js
+++ bloco_7/dia_3/pratica1.js
@@ -8,6 +8,7 @@ function sum(a, b) {
   return a + b;
 }
 
+assert.strictEqual(typeof sum, 'function');
 assert.strictEqual(sum(4,5), 9, 'A soma de 4 + 5 é 9');
 assert.strictEqual(sum(0,0), 0, 'A soma de 0 + 0 é 0');
 assert.throws(() => { sum(4,'5'); }, /^Error: parameters must be numbers$/)
