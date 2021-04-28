467e4539ee6638fdc55b83abcd676142048c64d4
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-28 15:59:31 -0300

    @

---
+++ bloco_7/dia_3/pratica3.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+assert.strictEqual(typeof myRemoveWithoutCopy, 'function');
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const arr = [5, 6, 7, 8]
+assert.deepStrictEqual(myRemoveWithoutCopy(arr, 5), [5, 6, 7, 8])
