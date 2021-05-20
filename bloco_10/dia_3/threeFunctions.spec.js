4d9fa4dfeb19da8ad9ea57b3709b65df74835c10
Jean Esteves <jean.esteves2001@gmail.com>
2021-05-20 19:33:37 -0300

    exercicio 5 feito

--- bloco_10/dia_3/threeFunctions.spec.js
+++ bloco_10/dia_3/threeFunctions.spec.js
@@ -1,13 +1,18 @@
 // Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.
 
-const { it } = require('@jest/globals');
+// Utilizando as mesmas funções do exercício anterior, repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar.
+
 const threeFunctions = require('./threeFunctions');
 
 describe('function uppercase', () => {
   it('toLowerCase', () => {
-    threeFunctions.uppercase = jest.fn().mockImplementation((str) => str.toLowerCase());
+    const uppercaseSpied = jest.spyOn(threeFunctions, 'uppercase').mockImplementation((str) => str.toLowerCase())
 
-    expect(threeFunctions.uppercase('OLA')).toBe('ola');
+    expect(uppercaseSpied('OLA')).toBe('ola');
+
+    threeFunctions.uppercase.mockRestore();
+
+    expect(threeFunctions.uppercase('ola')).toBe('OLA');
   })
 })
 
