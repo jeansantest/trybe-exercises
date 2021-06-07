10c43d093c3d7e8f298638588792b83070b0b4ae
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-07 11:41:56 -0300

    imagem mapa mental do react router

---
+++ FrontEnd/bloco_13/dia_2/exercises/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Olá Mundo</div>;
+  }
+}
+
+export default App;
