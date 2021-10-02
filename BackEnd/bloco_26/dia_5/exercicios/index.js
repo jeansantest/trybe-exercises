f367778846a9da44935a2d3ebca5ceeb96a3eaf8
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-02 16:23:43 -0300

    exercicio 2 feito

--- BackEnd/bloco_26/dia_5/exercicios/index.js
+++ BackEnd/bloco_26/dia_5/exercicios/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const userRouter = require('./routers/userRouter');
+const { isValidToken } = require('./middlewares/validations2');
+
 const PORT = 3000;
 
 const app = express();
@@ -10,5 +13,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/user', userRouter);
+
+app.get('/btc', isValidToken, async (_req, res) => {
+  const result = await axios.get(
+    'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+  );
+
+  res.status(200).json(result.data);
+});
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
